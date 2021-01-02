e7cf884d219ae89f8ab400a233905493c09f5064
famhy <fahmidouiri@gmail.com>
2021-01-02 12:37:00 +0100

    master

--- src/Components/Addtrans.jsx
+++ src/Components/Addtrans.jsx
@@ -1,23 +1,44 @@
+import React from "react";
 import "../Styles/Addtrans.css";
 
+import { connect } from "react-redux";
+import { addTodo } from "../Redux/action";
+
 function Addtran() {
+  const [transaction, setTransaction] = React.useState({
+    amount: 0,
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    setTransaction({ ...transaction, [e.target.name]: e.target.value });
+    console.log(transaction);
+  };
+
   return (
-    <div className="Addtran">
-        <h3>Add new transaction</h3>
-      <form >
+<div className="Addtran">
+      <h3>Add new transaction</h3>
+      <form>
         <div className="form-control">
-          <label htmlFor="text">Text</label>
-          <input type="text"   placeholder="Enter text..." />
+          <label htmlFor="text">Description</label>
+          <input
+            type="text"
+            placeholder="Enter text..."
+            name="description"
+            onChange={(e) => handleChange(e)}
+          />
         </div>
         <div className="form-control">
-          <label htmlFor="amount"
-            >Amount 
-            </label>
-          <input type="number"   placeholder="Enter amount..." />
+          <label htmlFor="amount">Amount</label>
+          <input
+            type="number"
+            placeholder="Enter amount..."
+            name="amount"
+            onChange={(e) => handleChange(e)}
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
-      <div>hello</div>
     </div>
   );
 }
---
+++ src/Redux/store.js
@@ -0,0 +1,8 @@
+import {createStore} from 'redux'
+
+import todos from './reducers/todos'
+
+const store =createStore(todos)
+
+
+export default store
